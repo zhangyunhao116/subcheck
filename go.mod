a36a7360bb87ed109f7e4ec65c693b82d7ea78cf
zhangyunhao <zhangyunhao116@gmail.com>
2023-03-25 02:25:00 +0000

    init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/zhangyunhao116/subcheck
+
+go 1.19
+
+require github.com/zhangyunhao116/skipset v0.13.0
+
+require github.com/zhangyunhao116/fastrand v0.2.1 // indirect
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/zhangyunhao116/skipset"
+)
+
+func main() {
+	var err error
+	curpath, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	allfiles := skipset.NewString()
+	err = filepath.Walk(curpath, func(inputpath string, info os.FileInfo, err error) error {
+		if err == nil && isVideo(inputpath) {
+			allfiles.Add(inputpath)
+		}
+		if err != nil {
+			panic(err)
+		}
+		if info.Size() == 0 {
+			panic("invalid size:" + inputpath)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	nosubtitle := skipset.NewString()
+	var wg sync.WaitGroup
+	for i := 0; i < runtime.GOMAXPROCS(-1); i++ {
+		wg.Add(1)
+		go func() {
+			allfiles.Range(func(value string) bool {
+				if allfiles.Remove(value) {
+					cmd := "ffprobe " + `"` + value + `"`
+					command := exec.Command("bash", "-c", cmd)
+					out, err := command.CombinedOutput()
+					if err != nil {
+						panic(err.Error() + "\n" + string(out))
+					}
+					ffprobeInfo := string(out)
+					if !strings.Contains(ffprobeInfo, "Subtitle:") {
+						nosubtitle.Add(value)
+						println(value)
+					}
+				}
+				return true
+			})
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	if nosubtitle.Len() != 0 {
+		println("Failed to fetch subtitle: ", nosubtitle.Len())
+	}
+}
+
+func isVideo(path string) bool {
+	var videos = []string{".mp4", ".mkv", ".webm"}
+	for _, v := range videos {
+		if strings.HasSuffix(path, v) {
+			return true
+		}
+	}
+	return false
+}
